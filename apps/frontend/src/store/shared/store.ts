7a1b408766a87321dcd9bfa483ce71621bc86f80
juanuranidev <juanuranidev@gmail.com>
2024-10-05 23:57:25 -0300

    Updated styles and store

--- apps/frontend/src/store/shared/store.ts
+++ apps/frontend/src/store/shared/store.ts
@@ -1,14 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "store/user/slices/user.slices";
 
 export const stores = configureStore({
-    reducer: {
-        user: userReducer,
-    },
-})
-
-
+  reducer: {
+    user: userReducer,
+  },
+});
 
 // Uncomment these lines to export types for better type inference
-export type RootState = ReturnType<typeof stores.getState>
-export type AppDispatch = typeof stores.dispatch
+export type RootState = ReturnType<typeof stores.getState>;
+export type AppDispatch = typeof stores.dispatch;
