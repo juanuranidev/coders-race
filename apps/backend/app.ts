eff46c0777a829b7a141392aff3275a6b05c21ec
juanuranidev <juanuranidev@gmail.com>
2024-10-21 23:51:42 -0300

    Updated initial config

---
+++ apps/backend/app.ts
@@ -0,0 +1,16 @@
+import { ServerRouter } from '@server/infrastructure/router/server.router';
+import { Server } from '@server/infrastructure/server';
+import ENVS from '@shared/infrastructure/config/envs/envs';
+
+(async () => {
+  main();
+})();
+
+function main() {
+  const server = new Server({
+    port: ENVS.PORT,
+    routes: ServerRouter.routes,
+  });
+
+  server.start();
+}
