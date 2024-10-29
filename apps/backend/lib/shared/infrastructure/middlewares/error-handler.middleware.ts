f4e1965318549484f6b2c87f53e5a3e0d2e32ca8
juanuranidev <juanuranidev@gmail.com>
2024-10-28 21:34:16 -0300

    Hotfix

--- apps/backend/lib/shared/infrastructure/middlewares/error-handler.middleware.ts
+++ apps/backend/lib/shared/infrastructure/middlewares/error-handler.middleware.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { getErrorInfo } from "@shared/domain/errors/error-catalog";
 
 export class ErrorHandlerMiddleware {
-  static async run(_req: Request, res: Response, err: Error) {
+  static async run(err: Error, req: Request, res: Response, next: NextFunction) {
     const { statusCode, message } = getErrorInfo(err);
 
     return res.status(statusCode).json({ error: message });
